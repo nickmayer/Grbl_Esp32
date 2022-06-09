0bccf309e59c2877eaf3b1201bfca8a35c8db590
Nick Mayer <nick@lastacorn.com>
2022-06-09 17:56:51 -0600

    Rewire everything

--- Grbl_Esp32/src/Machines/swidden.h
+++ Grbl_Esp32/src/Machines/swidden.h
@@ -31,39 +31,39 @@
 #define DEFAULT_STEP_PULSE_MICROSECONDS     3
 #define DEFAULT_STEPPER_IDLE_LOCK_TIME      255 //  255 = Keep steppers on
 
-#define DEFAULT_DIRECTION_INVERT_MASK    (bit(X_AXIS) | bit(Z_AXIS))
+#define DEFAULT_DIRECTION_INVERT_MASK    (bit(X_AXIS) | bit(Y_AXIS) | bit(Z_AXIS))
 
 // X:
 //  200 * 32 steps per rev (6400 steps per rev)
-//  20 tooth gear, 2mm belt pitch (40 mm per rev)
-//  6400/40 steps per mm (160)
+//  60 tooth motor 40 tooth screw, 5mm screw pitch (5*40/60 mm per rev)
+//  6400/3.3 steps per mm (213.33333)
 // Y:
-//  200 * 16 steps per rev (6400 steps per rev)
-//  20 tooth gear, 2mm belt pitch (40 mm per rev)
-//  3200/40 steps per mm (160)
+//  200 * 32 steps per rev (6400 steps per rev)
+//  60 tooth motor 40 tooth screw, 5mm screw pitch (5*60/40 mm per rev)
+//  6400/7.5 steps per mm (213.33333)
 // Z:
 //  200 * 8 steps pre rev (1600 steps per rev)
 //  8mm screw lead (8mm per rev)
 //  1600 / 8 steps per mm (200)
-#define DEFAULT_X_STEPS_PER_MM 160.0
-#define DEFAULT_Y_STEPS_PER_MM 160.0
-#define DEFAULT_Z_STEPS_PER_MM 200.0
+#define DEFAULT_X_STEPS_PER_MM (6400.0f / (10 / 3.0f))
+#define DEFAULT_Y_STEPS_PER_MM (6400.0f / (10 / 3.0f))
+#define DEFAULT_Z_STEPS_PER_MM (200.0f)
 
-#define DEFAULT_X_MAX_RATE 8000.0 // mm/min
-#define DEFAULT_Y_MAX_RATE 8000.0 // mm/min
-#define DEFAULT_Z_MAX_RATE 5000.0 // mm/min
+#define DEFAULT_X_MAX_RATE 7000.0 // mm/min
+#define DEFAULT_Y_MAX_RATE 7000.0 // mm/min
+#define DEFAULT_Z_MAX_RATE 4000.0 // mm/min
 
 #define DEFAULT_X_ACCELERATION 300.0 // mm/sec^2
 #define DEFAULT_Y_ACCELERATION 300.0 // mm/sec^2
-#define DEFAULT_Z_ACCELERATION 100.0 // mm/sec^2
+#define DEFAULT_Z_ACCELERATION 150.0 // mm/sec^2
 
 #define ENABLE_SOFTWARE_DEBOUNCE 1 // Limit switches seem to falsely trigger when router powers off
 #define DEFAULT_INVERT_LIMIT_PINS 0 // Normal Closed buttons
 #define DEFAULT_HARD_LIMIT_ENABLE 1
 #define DEFAULT_SOFT_LIMIT_ENABLE 1
 
-#define DEFAULT_X_MAX_TRAVEL 660.0 // mm $130 (0 to 660 right)
-#define DEFAULT_Y_MAX_TRAVEL 610.0 // mm $131 (0 front to 610 back)
+#define DEFAULT_X_MAX_TRAVEL 655.0 // mm $130 (0 to 690 right)
+#define DEFAULT_Y_MAX_TRAVEL 625.0 // mm $131 (0 front to 625 back)
 #define DEFAULT_Z_MAX_TRAVEL 225.0 // mm $132 (0 top to -225 bottom).
 
 #define DEFAULT_HOMING_ENABLE 1
@@ -77,27 +77,27 @@
 #define DEFAULT_HOMING_CYCLE_1 bit(Y_AXIS)
 #define DEFAULT_HOMING_CYCLE_2 bit(X_AXIS)
 
-// TODO: Add a NOT gate and invert this so open is disabled
-#define STEPPERS_DISABLE_PIN    GPIO_NUM_4
+// TODO: Add a NOT gate and invert this so open is disabled?
+#define STEPPERS_DISABLE_PIN    GPIO_NUM_15
 
 // Left/Right
 #define X_STEP_PIN              GPIO_NUM_16
-#define X_DIRECTION_PIN         GPIO_NUM_17
-#define X_LIMIT_PIN             GPIO_NUM_21
+#define X_DIRECTION_PIN         GPIO_NUM_5
+#define X_LIMIT_PIN             GPIO_NUM_32
 
 // Back/Forward
-#define Y_DIRECTION_PIN         GPIO_NUM_14
+#define Y_STEP_PIN              GPIO_NUM_19
+#define Y2_STEP_PIN             GPIO_NUM_17
+#define Y_DIRECTION_PIN         GPIO_NUM_18
 #define Y2_DIRECTION_PIN        Y_DIRECTION_PIN
-#define Y_STEP_PIN              GPIO_NUM_13
-#define Y2_STEP_PIN             GPIO_NUM_12
-#define Y_LIMIT_PIN             GPIO_NUM_27
+#define Y_LIMIT_PIN             GPIO_NUM_33
 
 // Up/Down
-#define Z_STEP_PIN              GPIO_NUM_25
-#define Z2_STEP_PIN             GPIO_NUM_33
-#define Z_DIRECTION_PIN         GPIO_NUM_32
+#define Z_STEP_PIN              GPIO_NUM_23
+#define Z2_STEP_PIN             GPIO_NUM_21
+#define Z_DIRECTION_PIN         GPIO_NUM_22
 #define Z2_DIRECTION_PIN        Z_DIRECTION_PIN
-#define Z_LIMIT_PIN             GPIO_NUM_22
+#define Z_LIMIT_PIN             GPIO_NUM_25
 
 #define DEFAULT_LASER_FULL_POWER    1000
 #define DEFAULT_SPINDLE_RPM_MAX     1000.0  // rpm $30
@@ -106,6 +106,7 @@
 #define DEFAULT_SPINDLE_MIN_VALUE   0.0     // $35 Percent of full period (extended set)
 #define DEFAULT_SPINDLE_MAX_VALUE   100.0   // $36 Percent of full period (extended set)
 
+// TODO: Add a NOT gate and invert this so open is disabled
 #define DEFAULT_INVERT_SPINDLE_ENABLE_PIN 0
 #define DEFAULT_SPINDLE_ENABLE_OFF_WITH_ZERO_SPEED 1
 
@@ -123,11 +124,12 @@ $Spindle/Type=RELAY
 
 #define DEFAULT_SPINDLE_FREQ    10000.0  // Hz ($33 extended set)
 #define SPINDLE_TYPE            SpindleType::LASER
-#define SPINDLE_OUTPUT_PIN      GPIO_NUM_2
-#define LASER_OUTPUT_PIN        GPIO_NUM_18
+#define SPINDLE_OUTPUT_PIN      GPIO_NUM_13
+
+#define LASER_OUTPUT_PIN        GPIO_NUM_27
 
 #define DEFAULT_INVERT_PROBE_PIN 1
-#define PROBE_PIN                GPIO_NUM_19
+#define PROBE_PIN                GPIO_NUM_26
 
 /*
 #define CONTROL_RESET_PIN       GPIO_NUM_34  // labeled Reset, needs external pullup
